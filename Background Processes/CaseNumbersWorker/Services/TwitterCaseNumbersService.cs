fc6f46faf8ebd0e039dff148a1a926cc48fc6f9f
Rhys Waters <info@rhyswaters.com>
2021-07-14 15:11:00 +0100

    test git

--- Background Processes/CaseNumbersWorker/Services/TwitterCaseNumbersService.cs
+++ Background Processes/CaseNumbersWorker/Services/TwitterCaseNumbersService.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using CaseNumbersWorker.Models;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
